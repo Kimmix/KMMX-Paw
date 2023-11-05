7f4f047121249937baf797f6d05e886b23e2023b
Kimmix <kimmix05@gmail.com>
2023-11-05 22:06:31 +0700

    use drv2605

--- src/haptic.h
+++ src/haptic.h
@@ -1,18 +1,22 @@
-const int hapticChannel = 0;
-const int motorPin = GPIO_NUM_2;
-const unsigned long duration = 40;
+#include "Haptic_DRV2605.h"
+Haptic_DRV2605 haptic;
+const int effectNo = 3;
 void setupMotor() {
-    ledcSetup(hapticChannel, 5000, 8);
-    // ledcAttachPin(ledPin, hapticChannel);
-    ledcAttachPin(motorPin, hapticChannel);
-    ledcWrite(hapticChannel, 200);
-    delay(100);
-    ledcWrite(hapticChannel, 0);
+    // initialize the Haptic controller
+    if (haptic.begin() != HAPTIC_SUCCESS) {
+        Serial.println("Haptic: Driver Error: DRV2605L Device not found - check your I2C connections?");
+    } else {
+        // okay correct chip, let's initialize it
+        Serial.println("Haptic: DeviceID = DRV2605L @ Address 0x5A was found! ");
+        // config the DRV2605 chip
+        haptic.setActuatorType(LRA);    // pick an actuator type
+        haptic.setMode(REGISTER_MODE);  // haptic effects triggered by I2C register write
+    }
+    haptic.setWaveform(0, effectNo);  // set the first sequence
+    // haptic.setWaveform(1, effectNo);  // end the sequence
+    haptic.goWait();                  // play the waveform
 }
 
 void motorVibrate() {
-    delay(50);
-    ledcWrite(hapticChannel, 255);
-    delay(duration);
-    ledcWrite(hapticChannel, 0);
+    haptic.goWait();  // play the waveform
 }
